f81924b30d0415125e15d9aa27a12f0ed91adcd3
github username <github email address>
2018-01-04 10:49:40 -0800

    Added routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+note = Note.create([{title: 'testing', summary: 'testing', img_url: nil}])
