ff3cc797f42edd750e3598bd0f54187545c968a9
github username <github email address>
2018-01-04 11:15:41 -0800

    added strong params for notes

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -4,8 +4,22 @@ class Api::NotesController < ApplicationController
     render json: Note.all
   end
 
+  def create
+    note = Note.new(note_params)
+    if note.save
+      render json: note
+    end
+  end
+
   def show
     render json: Note.find_by(id: params[:id])
+  end
+
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :summary, :img_url)
   end
 
 end
