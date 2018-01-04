35cf25efce4de5854209229e17f8cfde73630aac
github username <github email address>
2018-01-04 11:44:14 -0800

    completed back-end

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,4 +1,5 @@
 class Api::NotesController < ApplicationController
+  before_action :set_note, only: [:show, :update, :destroy]
 
   def index
     render json: Note.all
@@ -8,15 +9,37 @@ class Api::NotesController < ApplicationController
     note = Note.new(note_params)
     if note.save
       render json: note
+    else
+      render json: { message: @note.errors }, status: 400
     end
   end
 
   def show
-    render json: Note.find_by(id: params[:id])
+    render json: @note
+  end
+
+  def update
+    if @note.update(note_params)
+      render json: @note
+    else
+      render json: { message: @note.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @note.destroy
+      render status: 204
+    else
+      render json: { message: "Unsuccessful Delete" }, status: 400
+    end
   end
 
 
   private
+
+  def set_note
+    @note = Note.find_by(id: params[:id])
+  end
 
   def note_params
     params.require(:note).permit(:title, :summary, :img_url)
