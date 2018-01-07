4c78e75c5b14c0f04f108d3144b9face8389ed2f
github username <github email address>
2018-01-06 20:22:34 -0800

    added note form

---
+++ notes-client/src/components/notes/NoteForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+  class NoteForm extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        content: "",
+      }
+    }
+
+    handleChange = (event) => {
+      const { name, value} = event.target;
+
+      this.setState({[name]: value});
+    }
+
+    handleOnSubmit = (event) => {
+      event.preventDefault();
+      this.props.onSubmit(this.state);
+      this.setState({
+        content: ''
+      })
+    }
+
+    render(){
+      return (
+        <div>
+          <h4>Add a Note</h4>
+          <form className="NoteForm" onSubmit={(event) => this.handleOnSubmit(event)}>
+            <label htmlFor="content"><b>Notes:</b> </label>
+            <input
+              type="text"
+              name="content"
+              value={this.state.content}
+              onChange={(event) => this.handleChange(event)}
+            />
+            <br />
+            <input type="submit" />
+
+          </form>
+        </div>
+      )
+    }
+  }
+
+export default NoteForm;
