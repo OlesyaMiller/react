89d20bb3830b68c574dbb79818f748012671543e
github username <github email address>
2018-01-06 18:22:29 -0800

    added key value for booklist

--- notes-client/src/components/books/BookList.js
+++ notes-client/src/components/books/BookList.js
@@ -4,11 +4,11 @@ const BookList = (props) =>
   <div className="BookContainer">
   <h2> Your Notes </h2>
     <div>
-    {props.books.map(book =>
-      <div className="BookCard">
+    {props.books.map((book, index) =>
+      <div className="BookCard" key={index} >
         <h4> {book.title}</h4>
         <h5> {book.author}</h5>
-        <img className="BookImage" src={book.img_url} alt={book.title} />
+        <h5><img className="BookImage" src={book.img_url} alt={book.title}/> </h5>
       </div>
     )}
     </div>
