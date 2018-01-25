cfab35fd797e49a92a654eed613d586009055ef0
github username <github email address>
2018-01-24 22:10:52 -0800

    cleaned up book.js

--- notes-client/src/components/books/Book.js
+++ notes-client/src/components/books/Book.js
@@ -6,19 +6,9 @@ import {deleteBook} from '../../Actions/BookAction';
 import {fetchNotes} from '../../Actions/notesAction';
 import {increaseLike} from '../../Actions/BookAction';
 import {decreaseLike} from '../../Actions/BookAction';
-
-
 import Like from './like'
 
 class Book extends Component {
-
-  // constructor(props) {
-  //   super(props)
-  //
-  //   this.state = {
-  //     count: 0,
-  //   }
-  // }
 
   handleOnLike = () => {
     const likeInfo = {
@@ -39,39 +29,14 @@ class Book extends Component {
     }
   }
 
-  handleOnDeleteClick = () => {
-    this.props.deleteBook(this.props.book)
-  }
-
-  handleNoteClick = () => {
-    this.props.fetchNotes(this.props.book)
-  }
-
-  callApi = () => {
-    console.log('a')
-    fetch('http://localhost:3000/api/booksddddd')
-    .then(res => {
-      if (res.status !== 200) {
-        throw new Error(res.statusText)
-      } else {
-        console.log('b')
-        return res.json()
-      }
-    })
-    .then(data => console.log('c', data))
-    .catch(err => console.log('d', err))
-    console.log('e')
-  }
-
-
   render() {
     return (
       <div className="BookCard">
-        <button onClick={() => this.handleOnDeleteClick(this.props.book)}>x</button>
+        <button onClick={() => this.props.deleteBook(this.props.book)}>x</button>
         <h4> {this.props.book.title}</h4>
         <h5> {this.props.book.author}</h5>
         <h5><img className="BookImage" src={this.props.book.img_url} alt={this.props.book.title} /> </h5>
-        <Link to='/notes' onClick={() => this.handleNoteClick(this.props.book)} style={{cursor: 'pointer'}}>Notes</Link>
+        <Link to='/notes' onClick={() => this.props.fetchNotes(this.props.book)} style={{cursor: 'pointer'}}>Notes</Link>
         <br />
         < Like handleOnLike={this.handleOnLike}
         handleOnDislike={this.handleOnDislike}
